0b0156aaa1c8439531e6c450485a9f99ba9f916e
alanoodAlsh <94927860+alanoodAlsh@users.noreply.github.com>
2023-01-07 18:44:04 +0300

    changed the characters and lighting
    changed the character movement, lighting, and gravity

--- My project (2)/Assets/movement.cs
+++ My project (2)/Assets/movement.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     private CharacterController charactercontroller;
+    public Transform cam;
     public float Speed;
     public Animator anim;
     float Verticalmovement;
     float Horizontalmovement;
+    public float turnSmoothTime=0.1f;
+    float turnSmoothVelocity;
+
+    public float gravity=-9.81f;
+    Vector3 Velocity;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +29,19 @@ public class movement : MonoBehaviour
         anim.SetFloat("walkingHorizontal", Input.GetAxis("Horizontal"));
         anim.SetFloat("WalkingVertical", Input.GetAxis("Vertical"));
         Speed = 3;
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        movement.Normalize();
-        charactercontroller.Move(movement*Speed*Time.deltaTime);
-        if (movement != Vector3.zero)
-            transform.forward = movement;
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        
+        if (movement.magnitude>=0.1f){
+            
+                    float targetAngle=Mathf.Atan2(movement.x,movement.z)*Mathf.Rad2Deg+cam.eulerAngles.y;
+                    float angle=Mathf.SmoothDampAngle(transform.eulerAngles.y,targetAngle,ref turnSmoothVelocity,turnSmoothTime);
+                    transform.rotation=Quaternion.Euler(0f,angle,0f);
+
+                    Vector3 moveDir=Quaternion.Euler(0f,targetAngle,0f)*Vector3.forward;
+                    charactercontroller.Move(moveDir.normalized*Speed*Time.deltaTime);
+                    
+                    Velocity.y+=gravity*Time.deltaTime;
+                    charactercontroller.Move(Velocity*Time.deltaTime);
+    }
     }
 }
